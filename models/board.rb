a96ef67c32da010471638af49ad9555aad9cf363
Adrian <adrienfrancis.bourgeois@gmail.com>
2014-01-17 17:55:59 +1100

    print method for the board

--- models/board.rb
+++ models/board.rb
@@ -1,3 +1,6 @@
+require_relative 'coord.rb'
+require_relative 'square.rb'
+
 class Board
 
   attr_accessor :squares
@@ -8,9 +11,28 @@ class Board
       @squares[x] = {}
       (0..7).each do |y|
         coord = Coord.new x,y
-        @squares[x][y] =  Square.new coord
+        @squares[x][y] =  Square.new coord, self
       end
     end
   end
+
+  def print_schema
+    puts ""
+    (0..7).reverse_each do |y|
+      (0..7).each do |x|
+        if @squares[x][y].piece == nil
+          if (x+y)%2 == 0
+            print "\u2b1b "
+          else
+            print "\u2b1c "
+          end
+        else
+          print @squares[x][y].piece.symbol
+        end
+      end
+      puts ""
+    end
+  end
+
 
 end

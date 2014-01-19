b9b67d232410845c62020258e4cf1f6355e7858b
Adrian <adrienfrancis.bourgeois@gmail.com>
2014-01-19 13:52:49 +1100

    refractoring authorized_squares of rook

--- models/rook.rb
+++ models/rook.rb
@@ -33,62 +33,6 @@ class Rook < Piece
       end
     end
 
-    #(x+1..7).each do |cur_x|
-      #square_candidate = board.squares[cur_x][y]
-      #if (occuped = square_candidate.occuped?)
-        #if occuped == @color
-          #break
-        #else
-          #authorized_squares_array += [square_candidate]
-          #break
-        #end
-      #else
-        #authorized_squares_array += [square_candidate]
-      #end
-    #end
-
-    #(0..x-1).reverse_each do |cur_x|
-      #square_candidate = board.squares[cur_x][y]
-      #if (occuped = square_candidate.occuped?)
-        #if occuped == @color
-          #break
-        #else
-          #authorized_squares_array += [square_candidate]
-          #break
-        #end
-      #else
-        #authorized_squares_array += [square_candidate]
-      #end
-    #end
-
-    #(y+1..7).each do |cur_y|
-      #square_candidate = board.squares[x][cur_y]
-      #if (occuped = square_candidate.occuped?)
-        #if occuped == @color
-          #break
-        #else
-          #authorized_squares_array += [square_candidate]
-          #break
-        #end
-      #else
-        #authorized_squares_array += [square_candidate]
-      #end
-    #end
-
-    #(0..y-1).reverse_each do |cur_y|
-      #square_candidate = board.squares[x][cur_y]
-      #if (occuped = square_candidate.occuped?)
-        #if occuped == @color
-          #break
-        #else
-          #authorized_squares_array += [square_candidate]
-          #break
-        #end
-      #else
-        #authorized_squares_array += [square_candidate]
-      #end
-    #end
-
     return authorized_squares_array
   end
 
